874a331a1d3a2205fe21f373122357906643e192
tran dat <dattx@ominext.com>
2021-06-15 16:06:57 +0700

    finish login

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,6 +14,24 @@ window.Vue = require('vue').default;
 import User from "./Helpers/User";
 window.User = User;
 
+// Sweet Alert Start
+import Swal from 'sweetalert2'
+window.Swal = Swal
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+});
+window.Toast = Toast;
+// Sweet Alert End
+
 const router = new VueRouter({
     routes,
     mode: 'history'
