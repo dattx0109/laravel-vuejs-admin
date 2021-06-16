332baa2cc7b1f8dc13b8a0973d063aaebb09625a
tran dat <dattx@ominext.com>
2021-06-16 15:50:06 +0700

    validate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+        $this->middleware('JWT', ['except' => ['login', 'register']]);
     }
 
     /**
